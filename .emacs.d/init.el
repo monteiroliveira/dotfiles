7c8b1aa589be2be083148ffb594c32bae647d60d
Guilherme Monteiro de Oliveira <monteiro.oliveira1@outlook.com>
2023-04-12 14:33:38 +0000

    add dolist to change scale and color of the org-levels

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -222,6 +222,16 @@
   :custom
   (org-bullets-bullet-list '("◉" "●" "○" "◆" "●" "○" "◆")))
 
+(dolist (face '((org-level-1 1.7 ultra-bold "#51afef")
+		(org-level-2 1.6 extra-bold "#c678dd")
+		(org-level-3 1.5 bold       "#98be65")
+		(org-level-4 1.4 semi-bold  "#da8548")
+		(org-level-5 1.3 normal     "#5699af")
+		(org-level-6 1.2 normal     "#a9a1e1")
+		(org-level-7 1.1 normal     "#46d9ff")
+		(org-level-8 1.0 normal     "#ff6c6b")))
+  (set-face-attribute (nth 0 face) nil :font "Ubuntu Nerd Font" :weight (nth 2 face) :height (nth 1 face) :foreground (nth 3 face)))
+
 ;; Auto-generate toc
 (use-package toc-org
   :hook (org-mode . toc-org-enable))
