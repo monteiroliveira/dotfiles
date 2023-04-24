4dfc0f82fc963aff497df9302313f6043c4b318c
Guilherme Monteiro de Oliveira <monteiro.oliveira1@outlook.com>
2023-04-24 00:15:13 +0000

    add straight for better package manage

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -272,6 +272,20 @@
   :hook (org-mode . evil-org-mode))
 
 ;; DEVELOPMENT
+;; Straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;; Languages
 (use-package haskell-mode)
 (use-package lsp-haskell)
