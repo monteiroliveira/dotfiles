cf052de6fc30b7c2dac32db032f5cc1625d8932b
Guilherme Monteiro de Oliveira <gmonteiro.oliveira@outlook.com>
2023-10-14 22:30:17 -0300

    organize layouts and add more imports

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -2,19 +2,22 @@
 import XMonad
 
 -- Utils
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig (additionalKeysP, mkKeymap)
 import XMonad.Util.Ungrab
 import XMonad.Util.Loggers
 import XMonad.Util.SpawnOnce
 
 -- Hooks
+import XMonad.Hooks.ManageDocks (avoidStruts, docks, manageDocks, ToggleStruts(..))
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 
 -- Layout
+import XMonad.Layout.Renamed
 import XMonad.Layout.ShowWName
+import XMonad.Layout.Spacing
+import XMonad.Layout.LayoutModifier
 
 myModMask :: KeyMask
 myModMask = mod4Mask
@@ -31,12 +34,18 @@ myNormColor = "#282c34"
 myFocusColor :: String
 myFocusColor = "#46d9ff"
 
-myLayoutHook = tiled ||| Mirror tiled ||| Full
-  where
-    tiled   = Tall nmaster delta ratio
-    nmaster = 1      -- Default number of windows in the master pane
-    ratio   = 1/2    -- Default proportion of screen occupied by master pane
-    delta   = 3/100  -- Percent of screen to increment by when resizing panes
+mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+
+tall = renamed [Replace "tall"]
+       $ mySpacing 8
+       $ Tall 1 (3/100) (1/2)
+       --    Tall nmaster delta ratio
+       --    nmaster = 1      -- Default number of windows in the master pane
+       --    ratio   = 1/2    -- Default proportion of screen occupied by master pane
+       --    delta   = 3/100  -- Percent of screen to increment by when resizing panes
+
+myLayoutHook = avoidStruts $ tall ||| Mirror tall ||| Full
 
 myXmobarPP :: PP
 myXmobarPP = def
@@ -52,7 +61,8 @@ myStartupHook = do
   spawnOnce "nitrogen --restore &"
   spawnOnce "trayer --edge bottom --align right --widthtype request --SetDockType true --padding 6 --SetPartialStrut true --expand true --monitor eDP-1 --transparent true --alpha 0 --tint 0x282c34 --height 22"
 
-myWorkspaces = ["1:www", "2:dev", "3:doc", "4:sys", "5:mus", "6:vid", "7:gfx", "8:afk", "9:aux"]
+myWorkspaces = [" www ", " dev ", " doc ", " sys ", " mus ", " vid ", " gfx ", " afk ", " aux "]
+--myWorkspaces = [" 1:www ", " 2:dev ", " 3:doc ", " 4:sys ", " 5:mus ", " 6:vid ", " 7:gfx ", " 8:afk ", " 9:aux "]
 
 myShowWName :: SWNConfig
 myShowWName = def
