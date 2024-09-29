a988f7cc1813f3adeee062779fa53b12294cea30
Guilherme Monteiro de Oliveira <gmonteiro.oliveira@outlook.com>
2024-09-29 15:25:36 -0300

    update nvim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>ne", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><leader>", vim.cmd.so)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -15,6 +16,23 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Expand a project into tmux
+vim.keymap.set("n", "<C-f>", function()
+    if os.getenv("TMUX") ~= nil then
+        vim.cmd("silent !tmux neww tmuxprjmgr")
+        return
+    end
+    vim.cmd.terminal("tmuxprjmgr")
+end)
+
+-- Expand another section in tmux (this command inside a session is kind dumb)
+vim.keymap.set("n", "<C-i>", function()
+    if os.getenv("TMUX") ~= nil then
+        vim.cmd("silent !tmux neww tmuxsessmgr")
+        return
+    end
+end)
