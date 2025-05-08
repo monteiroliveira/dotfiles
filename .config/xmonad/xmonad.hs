991e0c22786e21cd31d8ec9dccf3c53139883706
Guilherme Monteiro de Oliveira <guilherme@monteiroliveira.com>
2025-05-08 17:12:04 -0300

    [feat]: update xmonad config

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,11 +1,19 @@
+import qualified Data.Map as M
+import Data.Tree
+import System.IO
 import XMonad
-import XMonad.Actions.GridSelect
+import XMonad.Actions.Navigation2D
+import XMonad.Actions.TiledWindowDragging
+import XMonad.Actions.TreeSelect
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks (ToggleStruts (..), avoidStruts, docks, manageDocks)
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.WindowSwallowing
+import XMonad.Layout.Accordion
+import XMonad.Layout.DraggingVisualizer
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.Renamed
@@ -19,10 +27,10 @@ import XMonad.Layout.WindowArranger (windowArrange)
 import XMonad.Layout.WindowNavigation
 import XMonad.Prompt
 import XMonad.Prompt.Shell (shellPrompt)
-import XMonad.Util.EZConfig (additionalKeysP, mkNamedKeymap)
-import XMonad.Util.Hacks (javaHack, trayerAboveXmobarEventHook, trayerPaddingXmobarEventHook, windowedFullscreenFixEventHook)
+import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings, mkKeymap)
+import XMonad.Util.Hacks (fixSteamFlicker, javaHack, trayerAboveXmobarEventHook, trayerPaddingXmobarEventHook, windowedFullscreenFixEventHook)
 import XMonad.Util.Loggers
-import XMonad.Util.NamedActions
+import XMonad.Util.Run
 import XMonad.Util.SpawnOnce
 
 myModMask :: KeyMask
@@ -54,18 +62,58 @@ myTabTheme =
       inactiveTextColor = "#FFFFFF"
     }
 
+myTreeSelectNavigation =
+  M.fromList
+    [ ((controlMask, xK_g), cancel),
+      ((controlMask, xK_p), movePrev),
+      ((controlMask, xK_n), moveNext),
+      ((controlMask, xK_b), moveParent),
+      ((controlMask, xK_f), moveChild),
+      ((controlMask, xK_o), moveHistBack),
+      ((controlMask, xK_i), moveHistForward)
+    ]
+
+myTreeSelectConfig =
+  TSConfig
+    { ts_hidechildren = True,
+      ts_background = 0xdd181818,
+      ts_font = "xft:Mononoki Nerd Font:size=12:antialias=true:hinting=true",
+      ts_node = (0xffd0d0d0, 0xff282828),
+      ts_nodealt = (0xffd0d0d0, 0xff181818),
+      ts_highlight = (0xffffffff, 0xff755999),
+      ts_extra = 0xffd0d0d0,
+      ts_node_width = 200,
+      ts_node_height = 30,
+      ts_originX = 0,
+      ts_originY = 0,
+      ts_indent = 80,
+      ts_navigate = myTreeSelectNavigation <+> defaultNavigation
+    }
+
+myTreeSelectForest :: Forest (TSNode (X ()))
+myTreeSelectForest =
+  [ Node
+      (TSNode "System Options >>" "Default System Options" (return ()))
+      [ Node (TSNode "Shutdown" "PowerOff the system" (spawn "poweroff")) [],
+        Node (TSNode "Screen Lock" "Lock the computer screen" (spawn "slock")) [],
+        Node (TSNode "Logoff" "Terminate user session" (spawn "loginctl terminate-user $(whoami)")) []
+      ]
+  ]
+
 tall =
   renamed [Replace "tall"] $
-    smartBorders $
-      windowNavigation $
-        mySpacing 8 $
-          ResizableTall 1 (3 / 100) (1 / 2) []
+    draggingVisualizer $
+      smartBorders $
+        windowNavigation $
+          mySpacing 8 $
+            ResizableTall 1 (3 / 100) (1 / 2) []
 
 bTall =
   renamed [Replace "bTall"] $
-    smartBorders $
-      windowNavigation $
-        ResizableTall 1 (3 / 100) (1 / 2) []
+    draggingVisualizer $
+      smartBorders $
+        windowNavigation $
+          ResizableTall 1 (3 / 100) (1 / 2) []
 
 floats =
   renamed [Replace "floats"] $
@@ -88,7 +136,7 @@ myLayoutHook =
   avoidStruts $
     windowArrange $
       toggleLayouts floats $
-        tall ||| bTall ||| floats ||| full ||| tabs
+        tall ||| bTall ||| floats ||| full ||| tabs ||| Accordion
 
 myWorkspaces :: [String]
 myWorkspaces = [" I ", " II ", " III ", " IV ", " V ", " VI ", " VII ", " VIII ", " IX "]
@@ -113,12 +161,11 @@ myManageHook =
 myXmobarPP :: PP
 myXmobarPP =
   def
-    { ppCurrent = xmobarColor "#3888BA" "" . wrap "{ " " }",
+    { ppCurrent = xmobarColor "#3888BA" "" . wrap "{" "}",
       ppVisible = xmobarColor "#BA8438" "",
       ppHidden = xmobarColor "#9E95C7" "" . wrap "*" "",
-      -- ppHiddenNoWindows = xmobarColor "#dfdfdf" "",
       ppTitle = xmobarColor "#FFFFFF" "" . shorten 75,
-      ppSep = "   "
+      ppSep = " : "
     }
 
 myStartupHook :: X ()
@@ -135,6 +182,7 @@ myStartupHook = do
       \--widthtype request --SetDockType true --SetPartialStrut true --expand true \
       \--monitor 1 --transparent true --alpha 0 --tint 0x181818 --height 27"
     )
+  setWMName "LG3D"
 
 myShowWName :: SWNConfig
 myShowWName =
@@ -159,73 +207,49 @@ myPromptConfig =
       position = Bottom
     }
 
-spawnSelected' :: [(String, String)] -> X ()
-spawnSelected' lst = gridselect conf lst >>= flip whenJust spawn
-  where
-    conf =
-      def
-        { gs_cellheight = 40,
-          gs_cellwidth = 180,
-          gs_cellpadding = 6,
-          gs_originFractX = 0.5,
-          gs_originFractY = 0.5,
-          gs_font = "xft:Mononoki Nerd Font:size=12:antialias=true:hinting=true"
-        }
-
-gsDefaults :: [(String, String)]
-gsDefaults =
-  [ ("Alacritty", "alacritty"),
-    ("Gimp", "gimp"),
-    ("Emacs", "emacs"),
-    ("Vlc", "vlc"),
-    ("Virt Manager", "virt-manager"),
-    ("Nvidia", "nvidia-settings"),
-    ("Thunar", "thunar"),
-    ("Discord", "discord"),
-    ("Signal", "signal-desktop")
+myKeyBindings :: [(String, X ())]
+myKeyBindings =
+  [ ("M-C-r", spawn "xmonad --recompile"),
+    ("M-S-r", spawn "xmonad --restart"),
+    ("M-<Return>", spawn myTerminal),
+    ("M1-S-c b", sendMessage ToggleStruts),
+    ("M1-S-c f", sendMessage ToggleLayout),
+    ("M-S-<Return>", shellPrompt myPromptConfig),
+    ("M1-S-<Return>", spawn "rofi -show drun -show-icons"),
+    ("M-S-t", treeSelect)
   ]
+  where
+    treeSelect = treeselectAction myTreeSelectConfig myTreeSelectForest
 
-myKeys :: XConfig l -> [((KeyMask, KeySym), NamedAction)]
-myKeys c =
-  let subKeys str ks = (subtitle str :) $ mkNamedKeymap c ks
-   in subKeys
-        "Xmonad Essentials"
-        [ ("M-C-r", addName "Recompile Xmonad" $ spawn "xmonad --recompile"),
-          ("M-S-r", addName "Restart Xmonad" $ spawn "xmonad --restart"),
-          ("M-S-<Return>", addName "Spawn Prompt" $ shellPrompt myPromptConfig),
-          ("M-<Return>", addName "Spawn Terminal" $ spawn myTerminal),
-          ("M1-S-<Return>", addName "Spawn Rofi" $ spawn "rofi -show drun -show-icons"),
-          ("M-c b", sendMessage' ToggleStruts),
-          ("M-c f", sendMessage' ToggleLayout)
-        ]
-        ^++^ subKeys
-          "Grid Select"
-          [ ("M-S-g", addName "Default GridSelect" $ spawnSelected' gsDefaults)
-          ]
+myMouseBindings :: [((ButtonMask, Button), Window -> X ())]
+myMouseBindings =
+  [((myModMask .|. shiftMask, button1), dragWindow)]
 
 mySwallowEventHook = swallowEventHook (className =? "Alacritty") (return True)
+
+my2DNavigations =
+  navigation2DP
+    def
+    ("w", "a", "s", "d")
+    [ ("M-", windowGo),
+      ("M-S-", windowSwap),
+      ("M-C-", windowToScreen)
+    ]
+    False
 
 myHandleEventHook =
   handleEventHook def
     <> trayerPaddingXmobarEventHook
     <> trayerAboveXmobarEventHook
     <> windowedFullscreenFixEventHook
+    <> fixSteamFlicker
     <> mySwallowEventHook
-
-xmobar0 :: StatusBarConfig
-xmobar0 = statusBarProp "xmobar -x 0 ~/.config/xmobar/xmobarrc" $ pure myXmobarPP
-
-xmobar1 :: StatusBarConfig
-xmobar1 = statusBarProp "xmobar -x 1 ~/.config/xmobar/xmobarrc" $ pure myXmobarPP
-
-xmobar2 :: StatusBarConfig
-xmobar2 = statusBarProp "xmobar -x 2 ~/.config/xmobar/xmobarrc" $ pure myXmobarPP
 
 main :: IO ()
 main = do
   xmonad $
-    addDescrKeys ((mod4Mask, xK_F1), xMessage) myKeys $
-      withSB (xmobar0 <> xmobar1 <> xmobar2) $
+    withSB (xmobar0 <> xmobar1 <> xmobar2) $
+      my2DNavigations $
         ewmhFullscreen $
           ewmh . docks $
             javaHack $
@@ -241,3 +265,9 @@ main = do
                   focusedBorderColor = myFocusColor,
                   handleEventHook = myHandleEventHook
                 }
+                `additionalMouseBindings` myMouseBindings
+                `additionalKeysP` myKeyBindings
+  where
+    xmobar0 = statusBarProp "xmobar -x 0 ~/.config/xmobar/xmobar.hs" $ pure myXmobarPP
+    xmobar1 = statusBarProp "xmobar -x 1 ~/.config/xmobar/xmobar.hs" $ pure myXmobarPP
+    xmobar2 = statusBarProp "xmobar -x 2 ~/.config/xmobar/xmobar.hs" $ pure myXmobarPP
