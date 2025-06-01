95816d7550adf15f1d080c9b291dccf08da35237
Guilherme Monteiro de Oliveira <guilherme@monteiroliveira.com>
2025-05-31 23:04:18 -0300

    [feat]: update some stuff in magit config

--- .emacs.d/lisp/+magit.el
+++ .emacs.d/lisp/+magit.el
@@ -10,7 +10,7 @@
 (defun magitc/gather-avaliables-worktrees ()
   "Stoled from https://github.com/magit/magit/blob/main/lisp/magit-worktree.el
 magit-worktree-status, but this just collect the available worktrees to use in
-the next functions"
+the next functions."
   (list (magit-completing-read
          "Worktrees"
          (cl-delete (directory-file-name (magit-toplevel))
@@ -25,7 +25,7 @@ the next functions"
 
 (defun magitc/magit-goto-worktree-file (worktree)
   "Jump to the current file in another worktree by relative path
-If the file exit so open the file with 'find-file' else open 'dired'"
+If the file exit so open the file with 'find-file' else open 'dired'."
   (interactive (magitc/gather-avaliables-worktrees))
   (let ((buf-file-name buffer-file-name)
         (line-pos (line-number-at-pos))
@@ -44,9 +44,30 @@ If the file exit so open the file with 'find-file' else open 'dired'"
                   (dired (file-name-directory new-file)))))
           (switch-to-buffer current-buffer)))))
 
+(defun magitc/add-raw-worktree (worktree)
+  "Create a raw worktree and open 'dired' the work tree is create
+in the bare repo root, OBS: this function works good only for my
+workflow, feel free to change it if necessary."
+  (interactive
+   (magit-completing-read "Worktree Name" nil))
+  (if-let ((repo-toplevel (magit-toplevel)))
+      (progn
+        (setq-local bare-root (file-name-directory repo-toplevel))
+        (if (not (magit-toplevel))
+            (setq-local bare-root repo-toplevel))
+        (progn
+          (shell-command
+           (format
+            "git worktree add %s"
+            worktree))
+          (dired bare-root)))
+    (message "Not in a git repo")))
+
 (global-set-key (kbd "C-c m w m") 'magit-worktree-move)
 (global-set-key (kbd "C-c m w b") 'magit-worktree-branch)
 (global-set-key (kbd "C-c m w c") 'magit-worktree-checkout)
+(global-set-key (kbd "C-c m w d") 'magit-worktree-delete)
+(global-set-key (kbd "C-c m w a") 'magitc/add-raw-worktree)
 (global-set-key (kbd "C-c m w g") 'magitc/magit-goto-worktree)
 (global-set-key (kbd "C-c m w G") 'magitc/magit-goto-worktree-file)
 
